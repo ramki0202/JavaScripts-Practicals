3ff18d9c8319ab4605032494fa3290fe06f7c99b
Ramki <ram.ramki0202@gmail.com>
2020-10-29 20:24:10 +0530

    inital

---
+++ 3.Oct.2020/29.Oct/main.js
@@ -0,0 +1,22 @@
+
+var first;
+first = 134;
+console.log(first);
+console.log(typeof first);
+var second;
+second = 'ramki';
+console.log(second);
+console.log(typeof second);
+var third;
+third = "ramki weds geetha";
+console.log(third);
+console.log(typeof third);
+var fourth;
+fourth = 100.25;
+console.log(fourth);
+console.log(typeof fourth);
+var fifth;
+fifth =" >" ;
+console.log(fifth);
+console.log(typeof fifth);
+
