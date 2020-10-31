c3b5787f7662a4d006504b1622bd33ea6df0ce7d
Ramki <ram.ramki0202@gmail.com>
2020-10-31 07:54:34 +0530

    calculator

--- 3.Oct.2020/31.Oct/main.js
+++ 3.Oct.2020/31.Oct/main.js
@@ -1,6 +1,27 @@
-function weding(){
-    a=prompt("Enter Hero Name");
-    b=prompt("Enter Heroin Name");
-    alert(a+" "+ "Weds" +" "+b);
+
+function add(a,b){
+    c=a+b;
+    alert("Your Ans is"+" "+c);
 }
-weding();
+function sub(a,b){
+    c=a-b;
+    alert("Your Ans is"+" "+c);
+}
+function mul(a,b){
+    c=a*b;
+    alert("Your Ans is"+" "+c);
+}
+function div(a,b){
+    c=a/b;
+    alert("Your Ans is"+" "+c);
+}
+function mod(a,b){
+    c=a%b;
+    alert("Your Ans is"+" "+c);
+}
+
+add(20,30);
+sub(20,30);
+mul(20,30);
+div(20,30);
+mod(20,30);
