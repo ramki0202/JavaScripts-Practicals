f239e555b89879839eb4067d2033697fbf8f02d2
Ramki <ram.ramki0202@gmail.com>
2020-10-31 08:14:13 +0530

    Functions WIth parameter

--- 3.Oct.2020/31.Oct/main.js
+++ 3.Oct.2020/31.Oct/main.js
@@ -1,27 +1,4 @@
-
-function add(a,b){
-    c=a+b;
-    alert("Your Ans is"+" "+c);
+function weding(hero,heroin){
+    alert(hero +" "+"Weds"+" "+heroin);
 }
-function sub(a,b){
-    c=a-b;
-    alert("Your Ans is"+" "+c);
-}
-function mul(a,b){
-    c=a*b;
-    alert("Your Ans is"+" "+c);
-}
-function div(a,b){
-    c=a/b;
-    alert("Your Ans is"+" "+c);
-}
-function mod(a,b){
-    c=a%b;
-    alert("Your Ans is"+" "+c);
-}
-
-add(20,30);
-sub(20,30);
-mul(20,30);
-div(20,30);
-mod(20,30);
+weding("Ramki","Geetha");
