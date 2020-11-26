185039c0f6b040d52fb0fafb47dbaa27960e5df3
Ramki <ram.ramki0202@gmail.com>
2020-11-26 10:16:53 +0530

    add parseInt

---
+++ 4.Nov/26.Nov/main.js
@@ -0,0 +1,10 @@
+
+function add(a,b) {
+     c=a+b;
+     return c;
+}
+
+var  opt1=parseInt(prompt("enter num1",""));
+var opt2=parseInt(prompt("enter num2",""));
+var ans=add(opt1,opt2);
+alert(ans);
