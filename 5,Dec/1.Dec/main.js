333540880f0a331f200b93925c834296b08b4ae4
Ramki <ram.ramki0202@gmail.com>
2020-12-07 20:07:49 +0530

    07.Dec.2020

--- 5,Dec/1.Dec/main.js
+++ 5,Dec/1.Dec/main.js
@@ -1,16 +1,7 @@
-function Employee(name,destignation,salary){
-     this.name = name;
-     this.destignation = destignation;
-     this.salary = salary;
+var array={
+     0:'Ramki';
+     1:"How are you";
+     2:27;
 }
 
-var employee1= new Employee('Ramki','devleloper',75000);
-console.log(employee1);
-
-var employee2= new Employee('kumar','director',85000);
-console.log(employee2);
-
-var employee3= new Employee('Sathees','Machanic',95000);
-console.log(employee3);
-
-
+console.log(array);
