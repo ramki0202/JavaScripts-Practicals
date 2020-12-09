7132483718db057a9e1ad1c9e4a25895c6c185d3
Ramki <ram.ramki0202@gmail.com>
2020-12-09 09:09:48 +0530

    7.Dec.2020

---
+++ 5,Dec/9.Dec/main.js
@@ -0,0 +1,14 @@
+var array={
+     0:'Ramki',
+     1:"How are you",
+     2:27,
+}
+
+console.log(array);
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
+
+
+var firstArray = new Array('Ramki',"how are you",27);
+console.log("firstAraay",firstArray);
