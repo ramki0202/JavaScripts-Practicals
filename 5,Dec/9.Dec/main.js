26d7f2a3a7fca5793eba7d3682cacd5c060edac4
Ramki <ram.ramki0202@gmail.com>
2020-12-10 19:28:37 +0530

    10.dec

--- 5,Dec/9.Dec/main.js
+++ 5,Dec/9.Dec/main.js
@@ -1,14 +1,1 @@
-var array={
-     0:'Ramki',
-     1:"How are you",
-     2:27,
-}
-
-console.log(array);
-console.log(array[0]);
-console.log(array[1]);
-console.log(array[2]);
-
-
-var firstArray = new Array('Ramki',"how are you",27);
-console.log("firstAraay",firstArray);
+document.getElementById("click").onclick=
