ccf4e5db8bebf9c9d32c11ea3f1ea7a74ba9b55c
Ramki <ram.ramki0202@gmail.com>
2021-03-09 14:55:08 +0530

    Java Script Advance Setup

--- 5,Dec/27.Dec/main.js
+++
@@ -1,6 +0,0 @@
-function display(n,a){
-    this.name=n;
-    this.age=a;
-    }
-    var s1 = new display("ramki",26);
-    document.getElementById("demo").innerHTML= "Person Name : " +s1.name + "<br> Person Age IS : " +s1.age;
