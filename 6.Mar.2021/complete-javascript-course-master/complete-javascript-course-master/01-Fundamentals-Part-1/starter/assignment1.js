d90fd44fee478e8d0668b328e00b5f5de573b104
Ramki <ram.ramki0202@gmail.com>
2021-03-11 10:09:50 +0530

    Data Type and typeof operator assignment

--- 6.Mar.2021/complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignment1.js
+++ 6.Mar.2021/complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignment1.js
@@ -5,3 +5,12 @@ var population = 170000000;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+let isIsland = true;
+let language;
+
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+console.log(typeof isIsland);
+console.log(typeof language);
