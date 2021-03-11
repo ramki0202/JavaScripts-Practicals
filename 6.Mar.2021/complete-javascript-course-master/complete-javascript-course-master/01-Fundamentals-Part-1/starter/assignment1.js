cd10de3cfd78f9acb40a781038a9d5626ba12fe7
Ramki <ram.ramki0202@gmail.com>
2021-03-11 17:07:41 +0530

    con,let,var

--- 6.Mar.2021/complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignment1.js
+++ 6.Mar.2021/complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignment1.js
@@ -1,16 +1,17 @@
-// var country = "India";
-// var continent = "Asia";
-// var population = 170000000;
+var country = "India";
+var continent = "Asia";
+var population = 170000000;
 
-// console.log(country);
-// console.log(continent);
-// console.log(population);
+console.log(country);
+console.log(continent);
+console.log(population);
 
-// let isIsland = true;
-// let language;
+let isIsland = true;
+let language;
 
-// console.log(typeof country);
-// console.log(typeof continent);
-// console.log(typeof population);
-// console.log(typeof isIsland);
-// console.log(typeof language);
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+console.log(typeof isIsland);
+console.log(typeof language);
+
