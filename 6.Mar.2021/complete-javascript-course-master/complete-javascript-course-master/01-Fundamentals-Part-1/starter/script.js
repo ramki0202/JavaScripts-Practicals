df9e1b61eba30fe955c13ac1bafee8454cf020ec
Ramki <ram.ramki0202@gmail.com>
2021-03-16 14:35:04 +0530

    switch case statement test

--- 6.Mar.2021/complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ 6.Mar.2021/complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -2,3 +2,34 @@
 // var heroine = "Geetha";
 
 // alert(hero + " " + "weds" + " " + heroine);
+let day = prompt("Enter the day..!");
+
+switch (day) {
+    case "Monday":
+    case "monday":
+        alert("Go To Gym..!");
+        break;
+    case "Tuesday":
+    case "tuesday":
+        alert("Go To Temple..!");
+        break;
+    case "Wednesday":
+    case "wednesday":
+        alert("Go To Coimbatore..!");
+        break;
+    case "Thursday":
+    case "thursday":
+        alert("Back to home....!");
+        break;
+    case "Friday":
+    case "friday":
+        alert("Drive a car...!");
+        break;
+    case "Saturday":
+    case "saturday":
+    case "Sunday":
+    case "sunday":
+        alert("Enjoy Weekend...!");
+    default:
+        alert("Enter correct day..!");
+}
