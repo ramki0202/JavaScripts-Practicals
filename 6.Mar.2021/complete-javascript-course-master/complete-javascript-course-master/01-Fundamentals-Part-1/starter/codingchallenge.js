c4459f954f8f783b4d863ad623a76b01eff99685
Ramki <ram.ramki0202@gmail.com>
2021-03-16 09:44:44 +0530

    Coding Challenge #3 Task 1 Completed

--- 6.Mar.2021/complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/codingchallenge.js
+++ 6.Mar.2021/complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/codingchallenge.js
@@ -13,5 +13,22 @@ else {
     console.log(`John's BMI(${johnHigherBMI}) is higher than Mark's BMI(${markHigherBMI})`);
 }*/
 
+const dolpinScore1 = 98;
+const dolpinScore2 = 108;
+const dolpinScore3 = 89;
+const kollasScore1 = 88;
+const kollasScore2 = 91;
+const kollasScore3 = 110;
+
+const dolpinScrore = dolpinScore1 + dolpinScore2 + dolpinScore3;
+const kollasScore = kollasScore1 + kollasScore2 + kollasScore3;
+
+const dolpinsScroreAvg = dolpinScrore / 3;
+const kollasScoreAvg = kollasScore / 3;
+if (dolpinsScroreAvg > kollasScoreAvg) {
+    console.log("The Winner of this competion is Dolpins ");
+} else {
+    console.log("The Winner of this competion is Kollas ");
+}
 
 
