86babadaed7a6f92d9ba074e71f40d46dfc9e9f2
Ramki <ram.ramki0202@gmail.com>
2021-04-04 12:54:26 +0530

    Commanded importaned stage

--- 6.Mar.2021/complete-javascript-course-master/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ 6.Mar.2021/complete-javascript-course-master/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,18 +1,23 @@
 'use strict';
 
+//variables diclared
 const number = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
+//query selector function for check button
 document.querySelector('.check').addEventListener('click', function () {
   const name = document.querySelector('.name').value;
   const ans = Number(document.querySelector('.guess').value);
   document.querySelector('.guess').value = number;
 
+  // if ans is 0 less than 0 is show not a number
   if (!ans || ans < 0) {
+    // if score is less than 1 user will loss game
     if (score <= 1) {
       document.querySelector('.message').textContent = 'Game Over😞';
       document.querySelector('.number').textContent = ans;
       const original = (document.querySelector('.guess').value = ans);
+      // if ans is 0 less than 0 is show not a number
     } else {
       document.querySelector(
         '.message'
@@ -22,6 +27,7 @@ document.querySelector('.check').addEventListener('click', function () {
       score--;
       document.querySelector('.score').textContent = score;
     }
+    // Main functionality working :if user enter low or high
   } else {
     if (ans == number) {
       document.querySelector(
