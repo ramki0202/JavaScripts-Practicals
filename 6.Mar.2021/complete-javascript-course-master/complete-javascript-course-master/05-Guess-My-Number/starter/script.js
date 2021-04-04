cd10005d6d3e737a8d36f3d4feabd6a4aad592be
Ramki <ram.ramki0202@gmail.com>
2021-04-04 12:46:07 +0530

    Game over Implemented

--- 6.Mar.2021/complete-javascript-course-master/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ 6.Mar.2021/complete-javascript-course-master/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -16,13 +16,19 @@ document.querySelector('.check').addEventListener('click', function () {
   document.querySelector('.guess').value = number;
 
   if (!ans || ans < 0) {
-    document.querySelector(
-      '.message'
-    ).textContent = `Sorry ${name}  it's Not a numbe⛔`;
-    document.querySelector('.number').textContent = ans;
-    const original = (document.querySelector('.guess').value = ans);
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score <= 1) {
+      document.querySelector('.message').textContent = 'Game Over😞';
+      document.querySelector('.number').textContent = ans;
+      const original = (document.querySelector('.guess').value = ans);
+    } else {
+      document.querySelector(
+        '.message'
+      ).textContent = `Sorry ${name}  it's Not a numbe⛔`;
+      document.querySelector('.number').textContent = ans;
+      const original = (document.querySelector('.guess').value = ans);
+      score--;
+      document.querySelector('.score').textContent = score;
+    }
   } else {
     if (ans == number) {
       document.querySelector(
@@ -30,21 +36,33 @@ document.querySelector('.check').addEventListener('click', function () {
       ).textContent = `hi ${name}  You Entered Correct Number🙌`;
       document.querySelector('.number').textContent = ans;
     } else if (ans < number) {
-      document.querySelector(
-        '.message'
-      ).textContent = `Sorry ${name}  You Entered too low😢`;
-      document.querySelector('.number').textContent = ans;
-      const original = (document.querySelector('.guess').value = ans);
-      score--;
-      document.querySelector('.score').textContent = score;
+      if (score <= 1) {
+        document.querySelector('.message').textContent = 'Game Over😞';
+        document.querySelector('.number').textContent = ans;
+        const original = (document.querySelector('.guess').value = ans);
+      } else {
+        document.querySelector(
+          '.message'
+        ).textContent = `Sorry ${name}  You Entered too low😢`;
+        document.querySelector('.number').textContent = ans;
+        const original = (document.querySelector('.guess').value = ans);
+        score--;
+        document.querySelector('.score').textContent = score;
+      }
     } else if (ans > number) {
-      document.querySelector(
-        '.message'
-      ).textContent = `Sorry ${name}  You Entered too High😢`;
-      document.querySelector('.number').textContent = ans;
-      const original = (document.querySelector('.guess').value = ans);
-      score--;
-      document.querySelector('.score').textContent = score;
+      if (score <= 1) {
+        document.querySelector('.message').textContent = 'Game Over😞';
+        document.querySelector('.number').textContent = ans;
+        const original = (document.querySelector('.guess').value = ans);
+      } else {
+        document.querySelector(
+          '.message'
+        ).textContent = `Sorry ${name}  You Entered too High😢`;
+        document.querySelector('.number').textContent = ans;
+        const original = (document.querySelector('.guess').value = ans);
+        score--;
+        document.querySelector('.score').textContent = score;
+      }
     }
   }
 });
