c71f1907c887e9d923008829e3d1ea37f54ed300
Ramki <ram.ramki0202@gmail.com>
2021-04-07 15:23:47 +0530

    Minimalist code

--- 6.Mar.2021/complete-javascript-course-master/complete-javascript-course-master/05-Guess-My-Number/starter/scripts/main.js
+++ 6.Mar.2021/complete-javascript-course-master/complete-javascript-course-master/05-Guess-My-Number/starter/scripts/main.js
@@ -1,92 +1,76 @@
-"use strict";
+'use strict';
 
+const message = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+//game over function
+const gameOver = function () {
+  document.querySelector('.message').style.fontSize = '30px';
+  document.querySelector('.message').textContent = `Game Over🎮`;
+  document.querySelector('.start-guess').src = 'images/tired.gif';
+  document.querySelector('body').style.backgroundColor = 'rgb(238, 77, 77)';
+  document.querySelector('.reset').style.backgroundColor = '#60b347';
+};
 //variable declaration
 let secretNumber = Math.trunc(Math.random() * 50) + 1;
-let userScore = Number((document.querySelector(".score").textContent = 5));
+let userScore = Number((document.querySelector('.score').textContent = 5));
 let highScore = 0;
 
 //We gona trigger check button
-document.querySelector(".check").addEventListener("click", function () {
-  let userName = document.querySelector(".userName").value;
-  let userGuess = Number(document.querySelector(".guess").value);
+document.querySelector('.check').addEventListener('click', function () {
+  //addEventListener variables
+  let userName = document.querySelector('.userName').value;
+  let userGuess = Number(document.querySelector('.guess').value);
+  let msgInput = function (value) {
+    userScore--;
+    document.querySelector('.score').textContent = userScore;
+    return `hi ${userName} ${value} `;
+  };
+  //main if else statement
   if (!userGuess || userGuess < 0 || userGuess > 50) {
     if (userScore <= 0) {
-      document.querySelector(".message").style.fontSize = "30px";
-      document.querySelector(".message").textContent = `Game Over🎮`;
-      document.querySelector(".start-guess").src = "images/tired.gif";
-      document.querySelector("body").style.backgroundColor = "rgb(238, 77, 77)";
-      document.querySelector(".reset").style.backgroundColor = "#60b347";
+      gameOver();
     } else {
-      document.querySelector(
-        ".message"
-      ).textContent = `hi ${userName} it's Not a Guess⛔`;
-      document.querySelector(".start-guess").src = "images/feel.gif";
-      userScore--;
-      document.querySelector(".score").textContent = userScore;
+      message(msgInput('Not a Guess⛔'));
+      document.querySelector('.start-guess').src = 'images/feel.gif';
     }
   } else {
     if (userGuess === secretNumber) {
       document.querySelector(
-        ".message"
+        '.message'
       ).textContent = `hi ${userName} Your Guess is Correct.!🥇`;
-      document.querySelector(".message").style.fontSize = "26px";
-      document.querySelector(".number").textContent = secretNumber;
-      document.querySelector(".start-guess").src = "images/claps.gif";
-      document.querySelector("body").style.backgroundColor = "#60b347";
+      document.querySelector('.message').style.fontSize = '26px';
+      document.querySelector('.number').textContent = secretNumber;
+      document.querySelector('.start-guess').src = 'images/claps.gif';
+      document.querySelector('body').style.backgroundColor = '#60b347';
       if (userScore > highScore) {
         highScore = userScore;
-        document.querySelector(".highscore").textContent = highScore;
+        document.querySelector('.highscore').textContent = highScore;
       }
-
-      if (userScore > highscore) {
-        highscore = userScore;
-        document.querySelector(".highscore").textContent = highscore;
-      }
-    } else if (userGuess < secretNumber) {
+    } else if (userGuess !== secretNumber) {
       if (userScore <= 1) {
-        document.querySelector(".message").style.fontSize = "30px";
-        document.querySelector(".message").textContent = `Game Over🎮`;
-        document.querySelector(".start-guess").src = "images/tired.gif";
-        document.querySelector("body").style.backgroundColor =
-          "rgb(238, 77, 77)";
-        document.querySelector(".reset").style.backgroundColor = "#60b347";
+        gameOver();
       } else {
-        document.querySelector(
-          ".message"
-        ).textContent = `hi ${userName} Your Guess is Too Low..!📉`;
-        document.querySelector(".start-guess").src = "images/sad.gif";
-        userScore--;
-        document.querySelector(".score").textContent = userScore;
-      }
-    } else if (userGuess > secretNumber) {
-      if (userScore <= 1) {
-        document.querySelector(".message").style.fontSize = "30px";
-        document.querySelector(".message").textContent = `Game Over🎮`;
-        document.querySelector(".start-guess").src = "images/tired.gif";
-        document.querySelector("body").style.backgroundColor =
-          "rgb(238, 77, 77)";
-        document.querySelector(".reset").style.backgroundColor = "#60b347";
-      } else {
-        document.querySelector(
-          ".message"
-        ).textContent = `hi ${userName} Your Guess is Too High..!📈`;
-        document.querySelector(".start-guess").src = "images/sad.gif";
-        userScore--;
-        document.querySelector(".score").textContent = userScore;
+        document.querySelector('.message').textContent =
+          userGuess > secretNumber
+            ? 'Your Guess is Too High..!📈'
+            : ' Your Guess is Too Low..!📈';
+        document.querySelector('.start-guess').src = 'images/sad.gif';
       }
     }
   }
 });
 
-document.querySelector(".reset").addEventListener("click", function () {
+//Reset button addEvent
+document.querySelector('.reset').addEventListener('click', function () {
   secretNumber = Math.trunc(Math.random() * 50) + 1;
-  document.querySelector(".message").textContent = "Start guessing...";
-  document.querySelector(".start-guess").src = "images/guess.gif";
-  document.querySelector(".number").textContent = "?";
-  document.querySelector("body").style.backgroundColor = "#222222";
-  document.querySelector(".guess").value = " ";
-  document.querySelector(".reset").style.backgroundColor = "rgb(216, 20, 20)";
-  document.querySelector(".message").style.fontSize = "16px";
-  userScore = Number((document.querySelector(".score").textContent = 5));
-  userName = document.querySelector(".userName").value = " ";
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.start-guess').src = 'images/guess.gif';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('body').style.backgroundColor = '#222222';
+  document.querySelector('.guess').value = ' ';
+  document.querySelector('.reset').style.backgroundColor = 'rgb(216, 20, 20)';
+  document.querySelector('.message').style.fontSize = '16px';
+  userScore = Number((document.querySelector('.score').textContent = 5));
+  userName = document.querySelector('.userName').value = ' ';
 });
