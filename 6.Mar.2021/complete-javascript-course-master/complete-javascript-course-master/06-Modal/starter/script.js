b617839c7e8148d61f5fd8c4f44d265c126e7c75
Ramki <ram.ramki0202@gmail.com>
2021-04-09 10:34:59 +0530

    Real time poppu project done

--- 6.Mar.2021/complete-javascript-course-master/complete-javascript-course-master/06-Modal/starter/script.js
+++ 6.Mar.2021/complete-javascript-course-master/complete-javascript-course-master/06-Modal/starter/script.js
@@ -1,8 +1,23 @@
 'use strict';
-const modelSHowBtn = document.querySelector('.show-modal');
-console.log(modelSHowBtn);
-const modelWindow = document.querySelector('.model');
-console.log(modelWindow);
-const modelCloseBtn = document.querySelector('.close-modal');
-console.log(modelCloseBtn);
+
+const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
+const modalCloseBtn = document.querySelector('.close-modal');
+const modalShowBtn = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < modalShowBtn.length; i++) {
+  modalShowBtn[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+modalCloseBtn.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+
+overlay.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
