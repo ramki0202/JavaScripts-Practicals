63a6c5a401ed8df5e3027455552bae95b1af5d2c
Ramki <ram.ramki0202@gmail.com>
2021-04-09 10:34:42 +0530

    Real time popup project done

--- 6.Mar.2021/complete-javascript-course-master/complete-javascript-course-master/06-Modal/starter/script.js
+++ 6.Mar.2021/complete-javascript-course-master/complete-javascript-course-master/06-Modal/starter/script.js
@@ -1,1 +1,8 @@
 'use strict';
+const modelSHowBtn = document.querySelector('.show-modal');
+console.log(modelSHowBtn);
+const modelWindow = document.querySelector('.model');
+console.log(modelWindow);
+const modelCloseBtn = document.querySelector('.close-modal');
+console.log(modelCloseBtn);
+const overlay = document.querySelector('.overlay');
