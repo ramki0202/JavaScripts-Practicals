97662ae8a6ecc1487d3d8f755b2ef4ea3a822de1
Ramki <ram.ramki0202@gmail.com>
2021-07-03 18:29:44 +0530

    Layout done

--- 6.Mar.2021/9/HTML Set up/scripts/main.js
+++ 6.Mar.2021/9/HTML Set up/scripts/main.js
@@ -1,7 +1,27 @@
-var js = 'Greate';
-if (js == 'Greate') {
-    alert("JavaScript is Awsome!");
-}
-var a = 520;
-var b = 20;
-console.log(a + b);
+"use strict";
+
+// const poll = {
+//   question: "What is your favourite programming language?",
+//   options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
+//   // This generates [0, 0, 0, 0]. More in the next section!
+//   answers: new Array(4).fill(0),
+//   registerNewAnswer() {
+//     const ans = Number(
+//       prompt("0: JavaScript 1: Python 2: Rust 3: C++ (Write option number)")
+//     );
+//     console.log(ans);
+
+//     this.answers[ans]++;
+//     console.log(this.answers);
+//   },
+// };
+
+// document
+//   .querySelector(".poll")
+//   .addEventListener("click", poll.registerNewAnswer.bind(poll));
+
+document.querySelector("poll2").textContent = 4;
+console.log(ram);
+
+// poll.registerNewAnswer();
+// console.log(poll.answers);
--- 6.Mar.2021/complete-javascript-course-master/complete-javascript-course-master/10-Functions/starter/script.js
+++ 6.Mar.2021/complete-javascript-course-master/complete-javascript-course-master/10-Functions/starter/script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const poll = {
+      question: "What is your favourite programming language?",
+      options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
+      // This generates [0, 0, 0, 0]. More in the next section!
+      answers: new Array(4).fill(0),
+      registerNewAnswer(){
+        const ans = Number(
+          prompt(`${this.question}\n ${this.options.join('\n') }\nwhat is your answer? `));
+        console.log(ans);
+     
+        this.answers[ans]++;
+        console.log(this.answers);
+        this.displayResults();
+        this.displayResults('string');
+      },
+      displayResults(type = 'array'){
+          if(type==='array'){
+              console.log(this.answers);
+          }else if(type === 'string'){
+              console.log(`poll result are ${this.answers.join(' ')}`);
+          }
+
+          },
+      
+    };
+
+document
+.querySelector('.poll')
+.addEventListener('click',poll.registerNewAnswer.
+bind(poll));
+
+poll.displayResults.call({answers:[1, 5, 3, 9, 6, 1]},'string')
